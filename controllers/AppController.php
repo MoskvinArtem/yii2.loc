86d01fb2fb03e7883aec881bbbe101f8103369ad
Moskvin Artem <a.moskvin@incom.kz>
2019-07-16 17:03:06 +0600

    #2 lesson 5

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -5,5 +5,9 @@ namespace app\controllers;
 use yii\web\Controller;
 
 class AppController extends Controller{
-  
+
+  public function debug($arr){
+    echo '<pre>' . print_r($arr) . '</pre>';
+  }
+
 }
--- controllers/PostController.php
+++ controllers/PostController.php
@@ -4,6 +4,9 @@ namespace app\controllers;
 
 class PostController extends AppController{
 
-  
+  public function actionTest(){
+    $vars = ['One','Punch','Man'];
+    return $this->render('test', compact('vars'));
+  }
 
 }
